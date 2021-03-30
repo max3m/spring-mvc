c131cb7ec68c176ad0ce5d76267eab3c1162a966
max3m <max-samara120@mail.ru>
2021-03-30 18:31:15 +0300

    changed root directory

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,23 +1,25 @@
 package web.controller;
 
-import model.Car;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import web.model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
-import service.CarServiceImpl;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarsController {
+    private final CarService carService;
+
+    public CarsController(CarService carService) {
+    this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CarServiceImpl.class);
-        CarService carService = (CarService) context.getBean("carServiceImpl");
         List<Car> cars = carService.showCars(count);
         model.addAttribute("cars", cars);
         return "cars";
